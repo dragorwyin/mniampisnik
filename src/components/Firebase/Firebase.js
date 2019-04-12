2312380a36aacf20ecd78c1f897042a9bbe07c60
Dawid Nowakowski <dn@idesign.center>
2019-04-12 22:38:53 +0200

    Add Firebase to project

--- src/components/Firebase/Firebase.js
+++ src/components/Firebase/Firebase.js
@@ -1,4 +1,6 @@
 import app from 'firebase/app';
+import React from 'react';
+import 'firebase/auth';
 
 const config = {
   apiKey: process.env.REACT_APP_API_KEY,
@@ -11,8 +13,20 @@ const config = {
 
 class Firebase {
   constructor() {
-    app.initializeApp(config);
-  }
+		app.initializeApp(config);
+		this.auth = app.auth();
+	}
+
+	login = (email, password) => this.auth.signInWithEmailAndPassword(email, password);
+	logout = () => this.auth.signOut();
+
+	/* not used yet */
+	register = (email, password) => this.auth.createUserWithEmailAndPassword(email, password);
+	resetPassword = email => this.auth.sendPasswordResetEmail(email);
+  updatePassword = password => this.auth.currentUser.updatePassword(password);
 }
 
+const FirebaseContext = React.createContext(null);
+
 export default Firebase;
+export { FirebaseContext };

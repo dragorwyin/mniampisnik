52dd9dd601676c1b659ad05e9758be352dff9f42
Dawid Nowakowski <dn@idesign.center>
2019-06-21 00:57:39 +0200

    Show build version

--- src/pages/layout/ProtectedPanel/index.js
+++ src/pages/layout/ProtectedPanel/index.js
@@ -5,6 +5,7 @@ import './styles.scss';
 import { connect } from 'react-redux';
 import Loader from '../../../components/common/Loader';
 import * as ROUTES from '../../../constants/routes';
+import preval from 'preval.macro'
 
 export const isAuth = auth => auth && auth.uid;
 
@@ -20,6 +21,11 @@ const ProtectedPanel = ({component: Component, loading, auth, ...rest}) => {
 						<main className="container" id="panel">
 							<Component {...props} {...rest} />
 						</main>
+						<small className="version">
+							Wersja z: {
+								preval`module.exports = new Date().toLocaleString('pl-PL', { hour12: false });`
+							}
+						</small>
 					</>
 				)
     )} />
